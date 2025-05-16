2e8fd7cb660ba24227ca5fc22fdeed272203ab66
Muthuthirumaran <tharanimtharan@gmail.com>
2025-05-15 20:50:49 -0400

    add react-icons dependency

--- src/app/(caseworker)/password-reset/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PasswordResetPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#F5F7FA]">
-      <div className="bg-white p-8 rounded shadow text-2xl font-bold text-[#003366]">
-        FORM
-      </div>
-    </div>
-  );
-} 
